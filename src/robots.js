8eece7a4cb038a95991caf0e5491d4c0d1dac71c
RC Apps <raul.calderon.chicago@gmail.com>
2024-04-04 19:28:39 -0500

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Papi Chulo',
+    username: 'pchulo',
+    email: 'pchulo@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ale Jandra',
+    username: 'Alex',
+    email: 'alex@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Nick Cal',
+    username: 'Nickal',
+    email: 'nickal@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Mamber Pamber',
+    username: 'mpamber',
+    email: 'mambipambi@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Stinky Stinkper',
+    username: 'Stinkinator',
+    email: 'stininkinator@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Jay Sus',
+    username: 'jaysus',
+    email: 'jsus@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Eych Man',
+    username: 'aman',
+    email: 'Eychman@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Aii Zilla',
+    username: 'azilla',
+    email: 'azilla@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Gman Alpha',
+    username: 'Finisher',
+    email: 'loveistheanswer@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Divine Techonologies',
+    username: 'Divinitech',
+    email: 'futureisnow@gmail.com'
+  }
+];
