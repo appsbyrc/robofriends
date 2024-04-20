befafeadb8f8d2fa77a9594ecf2139823f657e65
RC Apps <raul.calderon.chicago@gmail.com>
2024-04-20 01:06:20 -0500

    final product

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import Cardlist from './Cardlist';
-import SearchBox from './SearchBox';
-import Scroll from "./Scroll";
-import './App.css';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import Scroll from "../components/Scroll";
+import '../containers/App.css';
 
-//
 class App extends Component {
     constructor() {
         super()
@@ -13,24 +12,25 @@ class App extends Component {
             searchfield: ''
         }
     }
-
+    //componentDidMount methd is called after it's been rendered
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
 
-    }
+    }//onsearchchange method updates the searchfield state
+    //based on user input
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value });
     }
-
+    //The render method is where the component’s UI is defined.
     render() {
         const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         //below,if there's a delay display loading (if, else)  LOADING BAR
-        if (this.state.robots.length === 0) {
+        if (robots.length === 0) {
             return <h1>Loading</h1>
         } else {
             return !robots.length ?
@@ -50,3 +50,4 @@ class App extends Component {
 
 }
 export default App;
+
